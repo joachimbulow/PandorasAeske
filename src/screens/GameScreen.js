482462a30ac4a9264987d8003cdf1a711ff6edfd
Joachim <joachimbulow@hotmail.dk>
2021-05-05 23:19:39 +0200

    Animations added and working, needs cleanup

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -4,15 +4,23 @@ import { SvgXml } from "react-native-svg";
 import { BOX_SVG } from "../assets/svgs/Svgs.js";
 import PinkButton from "../components/PinkButton.js";
 import FirebaseService from "../services/FirebaseService.js";
+import LottieView from 'lottie-react-native';
 
 export default function QuestionsScreen(props) {
   const boxSvgMarkup = BOX_SVG;
 
   const [question, setQuestion] = useState("");
   const [myTurn, setMyTurn] = useState(false);
+  const [currentReader, setCurrentReader] = useState("")
+  const [showBoxAnimation, setShowBoxAnimation] = useState(false)
+  const [showSparkleAnimation, setShowSparkleAnimation] = useState(false)
 
   const participantsRef = useRef([]);
   const questionsRef = useRef(props.route.params.questions.sort(() => Math.random() - 0.5))
+
+  const boxOpeningAnimationRef = useRef(null)
+  const sparkleAnimationRef = useRef(null)
+
   const codeRef = useRef(props.route.params.code);
   const hostRef = useRef(props.route.params.host);
 
@@ -77,6 +85,16 @@ export default function QuestionsScreen(props) {
     }
   }
 
+  function enableBoxAnimation() {
+    setShowBoxAnimation(true)
+    setTimeout(() => setShowBoxAnimation(false), 4000)
+  }
+
+  function enableSparkleAnimation() {
+    setShowSparkleAnimation(true)
+    setTimeout(() => setShowSparkleAnimation(false), 4000)
+  }
+
   ////// Listeners
   function setupHostQuittingGameListener() {
     FirebaseService.getDatabaseReference("/" + codeRef.current).on("value", (snapshot) => {
@@ -111,10 +129,13 @@ export default function QuestionsScreen(props) {
     FirebaseService.getDatabaseReference("/" + codeRef.current + "/" + "currentTurn").on("value", (snapshot) => {
       if (snapshot.val() == props.route.params.name) {
         setMyTurn(true);
+        enableBoxAnimation()
       }
       else {
         setMyTurn(false)
+        enableSparkleAnimation()
       }
+      setCurrentReader(snapshot.val())
     })
   }
 
@@ -161,17 +182,42 @@ export default function QuestionsScreen(props) {
           {!myTurn && (
             <>
               <Text style={styles.turnToReadText}>
-                Det er en af de andre spilleres
+                Det er nu {currentReader}
               </Text>
-              <Text style={styles.turnToReadText}>tur til at læse op</Text>
+              <Text style={styles.turnToReadText}>der læser spørgsmålet op</Text>
             </>
           )}
         </View>
 
+        {myTurn &&
         <View style={styles.buttonsView}>
           {hostRef.current && <PinkButton onPress={() => proceedToNextQuestion()} text={questionsRef.current.length < 1 ? "AFSLUT SPIL" : "NÆSTE SPØRGSMÅL"} />}
         </View>
+        }
       </SafeAreaView>
+      {showBoxAnimation &&
+        <View style={styles.animationView}>
+          <Text style={styles.animationText}>Det er din tur til at læse op!</Text>
+          <LottieView
+            ref={animation => {
+              boxOpeningAnimationRef.current = animation;
+            }}
+            source={require('../assets/animations/box_animation.json')}
+            style={{ zIndex: 1000 }}
+            autoPlay
+          />
+        </View>}
+      {showSparkleAnimation &&
+        <View style={styles.animationView}>
+          <Text style={styles.animationText}>Nyt spørgsmål... Gør dig klar!</Text>
+          <LottieView
+            ref={animation => {
+              boxOpeningAnimationRef.current = animation;
+            }}
+            source={require('../assets/animations/32585-fireworks-display.json')}
+            autoPlay
+          />
+        </View>}
     </>
   );
 }
@@ -239,4 +285,24 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "center",
   },
+
+  animationView: {
+    position: 'absolute',
+    backgroundColor: '#F5FCFF88',
+    flex: 1,
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 2
+  },
+
+  animationText: {
+    marginBottom: 400,
+    fontFamily: "Roboto_100Thin",
+    color: "black",
+    fontSize: 24,
+  }
 });
